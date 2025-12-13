916c62d093ff2686f202ab8dce3efa62a569b7af
alvincpy120 <alvin.cheung@prologue.com>
2025-12-14 04:17:57 +0800

    added preview function

--- config/admin.js
+++ config/admin.js
@@ -14,4 +14,20 @@ module.exports = ({ env }) => ({
     nps: env.bool('FLAG_NPS', true),
     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
   },
+  preview: {
+    enabled: true,
+    config: {
+      allowedOrigins: env('CLIENT_URL'),
+      async handler(uid, { documentId, locale, status }) {
+        const document = await strapi.documents(uid).findOne({ documentId });
+        const slug = document?.slug;
+        
+        const previewSecret = env('PREVIEW_SECRET', 'strapi-preview-secret');
+        const clientUrl = env('CLIENT_URL');
+        
+        // Return the URL - Strapi will open it in a new tab
+        return `${clientUrl}/preview?secret=${previewSecret}&slug=${slug}`;
+      },
+    },
+  },
 });
